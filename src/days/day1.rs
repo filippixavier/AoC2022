9a5c83e963b0d829a5393736484f491a56d45a53
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2022-12-01 09:34:14 +0100

    Day 1 - Star 1

---
+++ src/days/day1.rs
@@ -0,0 +1,40 @@
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+fn get_input() -> Vec<Vec<usize>> {
+    let input = fs::read_to_string(Path::new("./input/day1.input"))
+        .expect("Something went wrong with the input");
+
+    let mut elves: Vec<Vec<usize>> = vec![];
+    let mut calories: Vec<usize> = vec![];
+    for line in input.trim().lines() {
+        if line.is_empty() {
+            elves.push(calories);
+            calories = vec![];
+            continue;
+        }
+        calories.push(line.trim().parse::<usize>().unwrap_or(0))
+    }
+
+    elves
+}
+
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let elves = get_input();
+    let elves_calories: Vec<usize> = elves
+        .iter()
+        .map(|inventory| inventory.iter().sum())
+        .collect();
+    println!(
+        "At most, an elf have {} calories",
+        elves_calories
+            .iter()
+            .fold(usize::MIN, |acc, calory| acc.max(*calory))
+    );
+    Ok(())
+}
+
+pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::error::Error;
 use std::io::{self};
+use std::time::Instant;
 
 type Star = fn() -> Result<(), Box<dyn Error + 'static>>;
 
-fn default_star() -> Result<(), Box<dyn Error + 'static>> {
-    println!("Invalid day or not published yet");
-    Ok(())
-}
+mod days;
 
 fn main() {
     let mut buffer = String::new();
@@ -29,14 +27,39 @@ fn main() {
     }
 
     match day {
+        1 => {
+            first_star = days::day1::first_star;
+            second_star = days::day1::second_star;
+        }
         _ => {
             println!("No day found matching input, exiting...");
-            first_star = default_star;
-            second_star = default_star;
             return;
         }
     }
 
-    first_star();
-    second_star();
+    let now = Instant::now();
+    match first_star() {
+        Err(x) => {
+            println!("Error: {:?}", x);
+        }
+        _ => {
+            println!("First star: Success!");
+        }
+    }
+
+    match second_star() {
+        Err(x) => {
+            println!("Error {:?}", x);
+        }
+        _ => {
+            println!("Second star: Success!");
+        }
+    }
+
+    let end = now.elapsed();
+    println!(
+        "Duration: {}sec {}millisec",
+        end.as_secs(),
+        end.subsec_millis()
+    );
 }
