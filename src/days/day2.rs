bfd1d636053a171fc2ffb32d1b6f7a223023668a
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2022-12-02 08:19:55 +0100

    Day 2 - Star 1

---
+++ src/days/day2.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+enum Round {
+    Rock(char),
+    Paper(char),
+    Scissors(char),
+}
+
+use Round::*;
+
+fn get_input() -> Vec<Round> {
+    fs::read_to_string(Path::new("./input/day2.input"))
+        .expect("Something went wrong with the input")
+        .trim()
+        .lines()
+        .map(|line| {
+            let round: Vec<char> = line.trim().chars().collect();
+            match round[0] {
+                'A' => Rock(round[2]),
+                'B' => Paper(round[2]),
+                'C' => Scissors(round[2]),
+                _ => unreachable!(),
+            }
+        })
+        .collect()
+}
+
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let scoring = HashMap::from([('X', 1), ('Y', 2), ('Z', 3)]);
+    let rounds = get_input();
+    let mut score: usize = 0;
+    for round in rounds {
+        match round {
+            Rock(other) => {
+                score += scoring.get(&other).unwrap_or(&0);
+                score += match other {
+                    'X' => 3,
+                    'Y' => 6,
+                    'Z' => 0,
+                    _ => unreachable!(),
+                }
+            }
+            Paper(other) => {
+                score += scoring.get(&other).unwrap_or(&0);
+                score += match other {
+                    'X' => 0,
+                    'Y' => 3,
+                    'Z' => 6,
+                    _ => unreachable!(),
+                }
+            }
+            Scissors(other) => {
+                score += scoring.get(&other).unwrap_or(&0);
+                score += match other {
+                    'X' => 6,
+                    'Y' => 0,
+                    'Z' => 3,
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
+    println!("According to strategy guide, I should get {}", score);
+    Ok(())
+}
+
+pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,10 @@ fn main() {
             first_star = days::day1::first_star;
             second_star = days::day1::second_star;
         }
+        2 => {
+            first_star = days::day2::first_star;
+            second_star = days::day2::second_star;
+        }
         _ => {
             println!("No day found matching input, exiting...");
             return;
