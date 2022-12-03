f3171ffa726039a3c2bc292f9616c4a1d2bd58fe
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2022-12-03 09:04:13 +0100

    Day 3 - Star 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -2,29 +2,30 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-fn get_input() -> Vec<(String, String)> {
+const VALUES: &str = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+fn get_input() -> Vec<String> {
     fs::read_to_string(Path::new("./input/day3.input"))
         .expect("Something went wrong with the input")
         .trim()
         .lines()
-        .map(|line| {
-            let count = line.chars().count();
-            (
-                line.chars().take(count / 2).collect(),
-                line.chars().skip(count / 2).collect(),
-            )
-        })
+        .map(String::from)
         .collect()
 }
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    let values = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let rucksacks = get_input();
     let mut score = 0;
-    for (part_1, part_2) in rucksacks {
+    for (part_1, part_2) in rucksacks.iter().map(|line| {
+        let count = line.chars().count();
+        (
+            line.chars().take(count / 2).collect::<String>(),
+            line.chars().skip(count / 2).collect::<String>(),
+        )
+    }) {
         for item in part_1.chars() {
             if part_2.contains(item) {
-                score += values.find(item).unwrap_or_default();
+                score += VALUES.find(item).unwrap_or_default();
                 break;
             }
         }
@@ -36,5 +37,17 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let rucksacks = get_input();
+    let mut score = 0;
+
+    for group in rucksacks.chunks(3) {
+        for item in group[0].chars() {
+            if group[1].contains(item) && group[2].contains(item) {
+                score += VALUES.find(item).unwrap_or_default();
+                break;
+            }
+        }
+    }
+    println!("The priority sum of all group badges is {}", score);
     Ok(())
 }
