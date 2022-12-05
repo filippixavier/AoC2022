1c7cdea79e27272938073128466d356876f4c2d7
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2022-12-05 11:25:01 +0100

    Day 5 - Star 1

---
+++ src/days/day5.rs
@@ -0,0 +1,73 @@
+use std::collections::VecDeque;
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+fn get_input() -> (Vec<VecDeque<char>>, Vec<[usize; 3]>) {
+    let input = fs::read_to_string(Path::new("./input/day5.input"))
+        .expect("Something went wrong with the input");
+    let blocks = input.trim_end().split("\r\n\r\n").collect::<Vec<_>>();
+
+    let max_index = blocks[0]
+        .trim_end()
+        .lines()
+        .last()
+        .unwrap()
+        .split(' ')
+        .last()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+
+    let mut stacks: Vec<VecDeque<char>> = vec![VecDeque::new(); max_index];
+
+    for line in blocks[0].lines() {
+        for (index, id) in line.chars().skip(1).step_by(4).enumerate() {
+            if id.is_alphabetic() {
+                stacks[index].push_front(id);
+            }
+            if id.is_numeric() {
+                break;
+            }
+        }
+    }
+
+    let instructions: Vec<[usize; 3]> = blocks[1]
+        .lines()
+        .map(|line| {
+            let mut inst = [0usize; 3];
+            let tmp = line.split(' ').collect::<Vec<_>>();
+            inst[0] = tmp[1].parse().unwrap();
+            inst[1] = tmp[3].parse().unwrap();
+            inst[2] = tmp[5].parse().unwrap();
+            inst
+        })
+        .collect();
+    (stacks, instructions)
+}
+
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let (mut stacks, instructions) = get_input();
+
+    for step in instructions {
+        for _ in 0..step[0] {
+            let id = stacks[step[1] - 1].pop_back().unwrap();
+            stacks[step[2] - 1].push_back(id);
+        }
+    }
+
+    let code = stacks.iter().fold(String::new(), |mut acc, ids| {
+        if let Some(id) = ids.back() {
+            acc.push(*id);
+        }
+        acc
+    });
+
+    println!("Crate code is: {}", code);
+
+    Ok(())
+}
+
+pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,10 @@ fn main() {
             first_star = days::day4::first_star;
             second_star = days::day4::second_star;
         }
+        5 => {
+            first_star = days::day5::first_star;
+            second_star = days::day5::second_star;
+        }
         _ => {
             println!("No day found matching input, exiting...");
             return;
